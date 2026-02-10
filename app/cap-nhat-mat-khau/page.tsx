96990226f40b6dc5a62f42e6a5b79e040a9df43c
hlong295 <121739523+hlong295@users.noreply.github.com>
2026-02-10 22:30:13 +0700

    cc

--- app/cap-nhat-mat-khau/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
-import CapNhatMatKhauClient from "./client";
-
-export default function CapNhatMatKhauPage() {
-  return <CapNhatMatKhauClient />;
-}
